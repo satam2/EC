d16c8c3bd8e71c4fd99b342b50c6cc98a24fe192
Sandy Tam <111902534+satam2@users.noreply.github.com>
2024-05-02 22:29:15 -0700

    Update fib.py

--- EC2/fib.py
+++ EC2/fib.py
@@ -1,1 +1,15 @@
-
+class fib():
+    def fibonacci(self, n):
+        if n <= 0:
+            return 0
+        elif n == 1:
+            return 1
+        
+        a, b = 0, 1
+        for _ in range(2, n + 1):
+            a, b = b, a + b
+        
+        return b
+    
+# test = fib()
+# print("the", str(10)+"th fibonacci number is", str(test.fibonacci(10)))
