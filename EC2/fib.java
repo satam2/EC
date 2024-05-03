68d977560ac12b90771bdc35a914315319ceb16e
Sandy Tam <111902534+satam2@users.noreply.github.com>
2024-05-02 22:28:54 -0700

    Update fib.java

--- EC2/fib.java
+++ EC2/fib.java
@@ -1,1 +1,23 @@
+public class fib {
+    public static int fibonacci(int n) {
+        if (n <= 0) {
+            return 0;
+        } else if (n == 1) {
+            return 1;
+        }
+        
+        int a = 0, b = 1;
+        for (int i = 2; i <= n; i++) {
+            int next = a + b;
+            a = b;
+            b = next;
+        }
+        return b;
+    }
+
+    public static void main(String[] args){
+        // fib test = new fib();
+        // System.out.println(test.fibonacci(3));
+    }
+}
 
